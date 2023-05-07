6e83d56ab8d5c6329a24df4ec48932f3d8373d06
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-08 01:28:33 +0300

    task 1
    1. Got the buttons' links.
    2. Created variables.
    3. Put event listeners on buttons.
    4 Created function on btn start and btn stop.
    5 created function with setInterval
    6. Added function with rendom colors.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const ref = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]")
+}
 
+const TIME_OUT_DELAY = 1000;
+const body = document.body.style;
+let intervalId = null;
+
+ref.btnStart.addEventListener("click", onStartButton);
+ref.btnStop.addEventListener("click", onStopButton);
+
+function onStartButton(){
+    body.background = getRandomHexColor()
+    changeBodyColorInterval()
+    ref.btnStart.setAttribute("disabled", "")
+}
+
+function onStopButton(){
+    clearInterval(intervalId)
+    body.background = "none"; 
+    ref.btnStart.removeAttribute("disabled")
+}
+
+function changeBodyColorInterval(){
+    intervalId = setInterval(()=>{
+        body.background = getRandomHexColor()
+    }, TIME_OUT_DELAY)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
