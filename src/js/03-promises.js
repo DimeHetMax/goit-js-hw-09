2a7bf51cf42e023cd935d3c343a7d427ebea3a7a
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-09 01:32:17 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  form: document.querySelector(".form")
+}
+refs.form.addEventListener("submit", onSubmitButton)
+
+function onSubmitButton(event){
+  event.preventDefault();
+  const elements = refs.form.elements;
+  let delay = Number(elements.delay.value)
+  const step = Number(elements.step.value)
+  const amount = Number(elements.amount.value)
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.5;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
 }
